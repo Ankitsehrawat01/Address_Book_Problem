ac627e5806b9241e340a5a2b7c3e4abeecc8c9a8
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-11 13:06:59 +0530

    create Contact

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Address_book;
+using System;
+
+namespace Address_book
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Addressbook add = new Addressbook();
+
+            add.AddContact();
+
+        }
+    }
+}
